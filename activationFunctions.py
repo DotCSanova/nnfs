e3246eaf0c442b1d5556a27113292fb8c4a8708a
DotCSanova <albertolopezcasanova@icloud.com>
2025-04-07 13:14:23 +0200

    Functions of loss, activation and layers

---
+++ activationFunctions.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# ReLU activation function
+class Activation_ReLU:
+
+    def forward(self, inputs):
+
+        self.output = np.maximum(0, inputs)
+
+# Softmax activation function
+class Activation_Softmax:
+
+    def forward(self, inputs):
+
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
+
+
+        
+
